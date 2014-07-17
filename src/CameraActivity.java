c2dc66174575b509aab0811bbefea32d1ceda120
michinggun <michinggun@gmail.com>
2014-07-18 01:29:06 +0700

    Preview done

--- src/CameraActivity.java
+++ src/CameraActivity.java
@@ -13,15 +13,15 @@ public class CameraActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+//        setContentView(R.layout.main);
+//
+//        // Create an instance of Camera
+//        mCamera = getCameraInstance();
+//
+//        // Create our Preview view and set it as the content of our activity.
+//        mPreview = new CameraPreview(this, mCamera);
+//        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+//        preview.addView(mPreview);
     }
     /** A safe way to get an instance of the Camera object. */
     public static Camera getCameraInstance(){
